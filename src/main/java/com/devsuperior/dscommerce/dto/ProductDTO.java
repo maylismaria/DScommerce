5fd5f5bad47f9aad9235f7cc3a976007d2542c8a
Maylis Maria <maylis.maria15@gmail.com>
2025-06-24 16:30:53 -0300

    Primeiro dto

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscommerce.dto;
+
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public class ProductDTO {
+
+
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    private String imgUrl;
+
+
+    public ProductDTO(){
+
+    }
+
+    public ProductDTO(Long id, String name, Double price, String description, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        imgUrl = entity.getImgUrl();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
