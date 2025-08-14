93464c221a81b91dcbc2e7a7b19bc94aedc0094d
Maylis Maria <maylis.maria15@gmail.com>
2025-08-14 18:47:21 -0300

    Dependecy bean vallidation

---
+++ src/main/java/com/devsuperior/dscommerce/service/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscommerce.service.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+
+}
