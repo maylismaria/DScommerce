fb87ee71183abbdb1addaf7ef76484e787a73a4e
Maylis Maria <maylis.maria15@gmail.com>
2025-11-03 20:13:25 -0300

    Adicionando controle de acesso por perfil e rota

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,6 +38,7 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity <ProductDTO > insert(@Valid @RequestBody ProductDTO dto)
     {
@@ -45,6 +47,8 @@ public class ProductController {
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity <ProductDTO > update(@PathVariable Long id, @Valid
     @RequestBody ProductDTO dto) {
@@ -52,12 +56,11 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
-
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping (value = "/{id}")
     public ResponseEntity<Void> update (@PathVariable Long id){
          service.delete(id);
         return ResponseEntity.noContent().build();
     }
-
 
 }
